995a9671d20ee632f416ae5dd79b03d67528a9cf
lrbbing <zjhhhhhhh@gmail.com>
2026-09-15 17:52:23 +0800

    Add layered variation to shell edge

--- sketch.js
+++ sketch.js
@@ -3,66 +3,65 @@ let edgeAmplitude = 8;
 
 let asymmetryFrequency = 2;
 let asymmetryAmplitude = 12;
+
 function setup() {
   createCanvas(700, 700);
 }
 
 function draw() {
   background(245);
-  //shell center
   noFill();
+
+  // shell center
   let centerX = width / 2;
   let centerY = height * 0.7;
 
   circle(centerX, centerY, 10);
 
+  // shell outline + radial ribs
   beginShape();
-
   vertex(centerX, centerY);
 
-
-
   for (let angle = -PI * 0.8; angle <= -PI * 0.2; angle += 0.05) {
- let radius = shellRadius(angle);
+    let radius = shellRadius(angle);
 
     let x = centerX + cos(angle) * radius;
     let y = centerY + sin(angle) * radius;
-strokeWeight(1.2);
+
+    strokeWeight(1.2);
     line(centerX, centerY, x, y);
 
     vertex(x, y);
   }
 
   vertex(centerX, centerY);
-
   endShape();
 
-  
+  // growth lines
   for (let t = 0.2; t < 1; t += 0.1) {
-  beginShape();
+    beginShape();
 
-  for (let angle = -PI * 0.8; angle <= -PI * 0.2; angle += 0.05) {
-    let radius = shellRadius(angle);
+    for (let angle = -PI * 0.8; angle <= -PI * 0.2; angle += 0.05) {
+      let radius = shellRadius(angle);
+      let growthRadius = radius * t;
 
-    let growthRadius = radius * t;
+      let x = centerX + cos(angle) * growthRadius;
+      let y = centerY + sin(angle) * growthRadius;
 
-    let x = centerX + cos(angle) * growthRadius;
-    let y = centerY + sin(angle) * growthRadius;
-strokeWeight(0.6);
-    vertex(x, y);
+      strokeWeight(0.6);
+      vertex(x, y);
+    }
+
+    endShape();
   }
+}
 
-  endShape();
-}
-}
 function shellRadius(angle) {
   let centerBoost = cos(angle + HALF_PI) * 80;
 
-  let edgeWave =
-    sin(angle * edgeFrequency) * edgeAmplitude;
+  let edgeWave = sin(angle * edgeFrequency) * edgeAmplitude;
 
-  let asymmetry =
-    sin(angle * asymmetryFrequency + 0.8) * asymmetryAmplitude;
+  let asymmetry = sin(angle * asymmetryFrequency + 0.8) * asymmetryAmplitude;
 
   return 220 + centerBoost + edgeWave + asymmetry;
 }
