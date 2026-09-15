b5db73b2f9740d0dc1ed039f09aea25eaeaf5b5e
lrbbing <zjhhhhhhh@gmail.com>
2026-09-15 17:18:22 +0800

    Add variable shell rib lengths

--- sketch.js
+++ sketch.js
@@ -4,14 +4,16 @@ function setup() {
 
 function draw() {
   background(245);
-
+  //shell center
   let cx = width / 2;
   let cy = height * 0.7;
-  let radius = 250;
 
   circle(cx, cy, 10);
 
+  // draw radial ribs
   for (let angle = -PI * 0.8; angle <= -PI * 0.2; angle += 0.1) {
+    let centerBoost = cos(angle + HALF_PI) * 80;
+    let radius = 220 + centerBoost;
 
     let x = cx + cos(angle) * radius;
     let y = cy + sin(angle) * radius;
